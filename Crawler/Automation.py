1448a971d8f4325ae44bb40109fe8968ce4e25c8
puzh5232@colorado.edu <puzh5232@colorado.edu>
2020-07-04 21:30:45 -0600

    retrieve online data

--- Crawler/Automation.py
+++ Crawler/Automation.py
@@ -6,13 +6,12 @@ def main():
     config = json.load(open(".\\Tools\\Config.json", "r"))
     browser = BasePage()
     browser.open_explore(config['target_url'], ".\\Tools", config['user_agent'], config['screen_width'], config['screen_height'], config['pixel_ratio'])
+    result = browser.get_all_data()
 
-    
+    with open(".\\result.json", "w") as ofs:
+        ofs.write(json.dumps(result))
 
-
-
-
-    time.sleep(2)
+    browser.close_explore()
 
 
 if __name__ == '__main__':
--- Crawler/base_page.py
+++ Crawler/base_page.py
@@ -8,6 +8,7 @@ from random import randint
 from selenium import webdriver as im_webdriver
 from selenium.webdriver.support import expected_conditions as im_expected_conditions
 from selenium.common import exceptions as im_selexc
+from selenium.webdriver.common.touch_actions import TouchActions
 
 
 class BasePage:
@@ -34,6 +35,9 @@ class BasePage:
         
         my_chrome_option.add_experimental_option('w3c', False)
         my_chrome_option.add_argument('--log-level=3')
+        my_chrome_option.add_argument('--headless')
+        my_chrome_option.add_argument('--disable-gpu')
+        my_chrome_option.add_argument('--disable-software-rasterizer')
         my_chrome_option.add_argument('--disable-smooth-scrolling')
         my_chrome_option.add_argument('--use-fake-mjpeg-decode-accelerator')
         my_chrome_option.add_argument('--use-file-for-fake-audio-capture')
@@ -70,3 +74,30 @@ class BasePage:
                break
 
             time.sleep(1)
+
+
+    def get_all_data(self):
+        table = self.web_driver.find_element_by_xpath('//*[@id="thetable"]/tbody')
+        rows = table.find_elements_by_tag_name('tr')
+
+        result = list()
+
+        for row_idx in range(0, len(rows)):
+            ths = rows[row_idx].find_elements_by_tag_name('th')
+            tds = rows[row_idx].find_elements_by_tag_name('td')
+
+            if len(ths) < 2 or len(tds) < 3:
+                continue
+            
+            try:
+                country = ths[1].find_element_by_tag_name('a').get_attribute('textContent')
+                cases = re.sub('[\xc2\xa0]', ' ', re.sub('[\r\n\t]', '', tds[0].get_attribute('textContent')))
+                deaths = re.sub('[\xc2\xa0]', ' ', re.sub('[\r\n\t]', '', tds[1].get_attribute('textContent')))
+                recover = re.sub('[\xc2\xa0]', ' ', re.sub('[\r\n\t]', '', tds[2].get_attribute('textContent')))
+                
+                result.append({"Country": country, "Cases": cases, "Deaths": deaths, "Recover": recover})
+
+            except im_selexc.StaleElementReferenceException:
+                continue
+
+        return result
