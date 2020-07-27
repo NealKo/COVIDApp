ca0f37426bafd476ae1f67bdc7494bb3a01e90f7
neal <neal.kornreich@gmail.com>
2020-07-27 18:38:00 +0300

    IS working without main

--- flask_setup/main_flask.py
+++ flask_setup/main_flask.py
@@ -1,49 +1,63 @@
 from flask import Flask, render_template
 from flask import make_response
 import pandas as pd
+import numpy as np
 
 from Repo.Repo import Repo
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/Home')
 @app.route('/home')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/About')
 @app.route('/about')
 def about():
-	return render_template('about.html', title='About')
+    return render_template('about.html', title='About')
+
 
 @app.route('/Updates')
 @app.route('/updates')
 def updates():
-	return render_template('updates.html', title='Updates')
+    return render_template('updates.html', title='Updates')
+
 
 @app.route('/Facts')
 @app.route('/facts')
 def facts():
-	return render_template('facts.html', title='Facts')
+    return render_template('facts.html', title='Facts')
 
 
 @app.route('/Links')
 @app.route('/links')
 def links():
-	return render_template('links.html', title="Links")
+    return render_template('links.html', title="Links")
+
 
 # This is for only testing display of tables
 @app.route('/table')
 @app.route('/Table')
 def displayTables():
-	df = Repo.get_formatted_data()
-	return render_template('table.html',  tables=[df.to_html(classes='data', header="true", index=False)])
+    df = Repo.get_formatted_data()
+    return render_template('table.html',
+                           data=df.to_html(index=False, table_id="ctable"))
 
-#Redirect for all routes not in the app.
-#make_response simplifies the error message process.
+
+@app.route('/testing')
+def testTables():
+    x = pd.DataFrame(np.random.randn(20, 5))
+    return render_template("test.html", data=x.to_html(table_id="example"))
+
+
+# Redirect for all routes not in the app.
+# make_response simplifies the error message process.
 @app.route('/<p_n>')
 def other_page(p_n):
-    	response = make_response('The page named %s does not exist.' \
+    response = make_response('The page named %s does not exist.' \
                              % p_n, 404)
-    	return response
+    return response
